ad3cf7d710a9779bed7f055e9b7c8e946ebb2e6f
tomizdebski <tomizdebski@gmail.com>
2023-09-17 12:24:59 +0200

    set_config_next_img

--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-//     images: {
+/** @type {import('next').NextConfig} */
+module.exports = {
+    images: {
 
      
 
-//       // formats: ['image/jpg', 'image/webp', 'image/png'],
+      // formats: ['image/jpg', 'image/webp', 'image/png'],
 
-//       remotePatterns: [
-//         {
-//           protocol: 'https',
-//           hostname: 'barter-api-db-neon.onrender.com',
-//           port: '',
-//           pathname: '/**',
-//         },
-//       ],
-//     },
-//   }
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'barter-api-db-neon.onrender.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
   // localhost
 
-/** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    //formats: ['image/jpg', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '4000',
-        pathname: '/**',
-      },
-    ],
-  },
-}
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   images: {
+//     //formats: ['image/jpg', 'image/webp'],
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         port: '4000',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// }
 
 
 
