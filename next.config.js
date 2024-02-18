094cfc5fa8f654cc55dc3331a4a0c0c00c661ba5
tomizdebski <tomizdebski@gmail.com>
2024-02-18 12:47:51 +0100

    fix_images

--- next.config.js
+++ next.config.js
@@ -11,23 +11,5 @@ module.exports = {
       ],
     },
   }
-  //localhost
-
 /** 
- * 
-// @type {import('next').NextConfig} */
-// module.exports = {
-//   images: {
-//     //formats: ['image/jpg', 'image/webp'],
-//     remotePatterns: [
-//       {
-//         protocol: 'http',
-//         hostname: 'localhost',
-//         port: '4000',
-//         pathname: '/**',
-//       },
-//     ],
-//   },
-// }
-
-
+*/
