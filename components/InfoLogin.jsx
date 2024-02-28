1ac3cdf47ff03b6bcdc70b1cebe9d2567fc637a6
tomizdebski <tomizdebski@gmail.com>
2024-02-28 20:53:29 +0100

    update_info_text

--- components/InfoLogin.jsx
+++ components/InfoLogin.jsx
@@ -9,6 +9,7 @@ const InfoLogin = () => {
 
   if (!userInfo) return (
     <div className="flex bg-gray-300 justify-end font-saira gap-4">
+      <p>Pierwsze uruchomienie trwa 2 min bo działam na darmowym planie</p>
       <p>Test login data:</p>
       <p>Username: testuser</p>
       <p>Password: testpassword</p>
